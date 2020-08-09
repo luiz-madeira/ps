662e37138218012b5aef5a4fe7a1f874f8ba0207
Luiz Madeira <luiz@madeiracampos.com>
2020-08-09 14:08:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>AWS CloudFormation PHP Sample</title>
+    <title>AWS PHP Sample</title>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   </head>
   <body>
